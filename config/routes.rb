296999db0c729c827126aa74a2e91ccbb827d97f
Wagner Renzi <wrenzi@gmail.com>
2015-01-05 16:50:02 -0200

    Chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
  
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'store#index', as: 'store'
 
