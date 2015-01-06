94e954980bddc0c0facd92a5d4ff492236578bb5
Wagner Renzi <wrenzi@gmail.com>
2015-01-06 08:55:32 -0200

    Chapter 13

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,16 +39,12 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-    
-        format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
-        format.json { render action: 'show', status: :created,
-          location: @order }
-      
+        OrderNotifier.received(@order).deliver
+        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.json { render action: 'show', status: :created, location: @order }
       else
         format.html { render action: 'new' }
-        format.json { render json: @order.errors,
-          status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,20 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+ config.action_mailer.delivery_method = :smtp
+
+ config.action_mailer.smtp_settings = {
+    :address              => "smtp.zoho.com", 
+    :port                 => 465,                 
+    :user_name            => 'contact@memoforever.com',
+    :password             => 'contact229!#',         
+    :authentication       => :login,
+    :ssl                  => true,
+    :tls                  => true,
+    :enable_starttls_auto => true    
+}
+  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -34,4 +48,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+
+
+
+
+
 end
